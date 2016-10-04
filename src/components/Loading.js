60b949608912dcd4fbc126dedcac864a6df61a89
n3tr <saakyz@gmail.com>
2016-10-04 20:54:11 +0700

    Add List Loading

---
+++ src/components/Loading.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default () => (
+  <div className="loading"><i className="fa fa-spinner fa-spin fa-3x fa-fw"></i></div>
+)
--- src/pages/PopularShots.js
+++ src/pages/PopularShots.js
@@ -1,12 +1,41 @@
 import React from 'react'
 import ShotList from '../components/ShotList'
-import ShotData from '../data/shots.json'
+import Loading from '../components/Loading'
+import fetchPopularShot from '../api/fetchPopularShot'
 
 export default class PopularShots extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      shots: [],
+      loading: false
+    }
+  }
+
+  componentDidMount() {
+    this.fetchShots()
+  }
+
+  fetchShots() {
+    this.setState({
+      loading: true
+    })
+    fetchPopularShot().then((shots) => {
+      this.setState({
+        shots,
+        loading: false
+      })
+    })
+  }
+
   render() {
+
     return (
       <div id="main" className="container clearfix">
-        <ShotList shots={ShotData} />
+      {
+        this.state.loading ? <Loading /> : <ShotList shots={this.state.shots} />
+      }
       </div>
     )
   }
